0a80fc3b57fe2558d87c11bee6493df186c384f1
kaeru-cinna <yosiki.4979@gmail.com>
2026-01-23 19:37:12 +0900

    Add files via upload

--- include.js
+++ include.js
@@ -4,6 +4,13 @@ fetch("header.html", { cache: "force-cache" })
   .then(html => {
     document.getElementById("header").innerHTML = html;
   });
+  
+/* ▼ 目次読み込み（キャッシュ活用） */
+fetch("Contents.html", { cache: "force-cache" })
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("Contents").innerHTML = html;
+  });
 
 /* ▼ 目次折りたたみ制御 */
 const toc = document.getElementById("toc");
