730b95f5b3748355d07e74293f080bcfda172e20
kaeru-cinna <yosiki.4979@gmail.com>
2026-01-23 20:04:20 +0900

    Add files via upload

---
+++ include.js
@@ -0,0 +1,25 @@
+/* ヘッダー */
+fetch("header.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("header").innerHTML = html;
+  });
+
+/* 目次 */
+fetch("toc.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("toc-area").innerHTML = html;
+  });
+
+/* 目次折りたたみ */
+window.addEventListener("scroll", () => {
+  const toc = document.getElementById("toc");
+  if (!toc) return;
+
+  if (window.scrollY > 300) {
+    toc.classList.add("collapsed");
+  } else {
+    toc.classList.remove("collapsed");
+  }
+});
