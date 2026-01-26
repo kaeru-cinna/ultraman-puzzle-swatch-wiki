25bb5a3f0cdcb4073bd85478a2e2a2b240bbf923
kaeru-cinna <yosiki.4979@gmail.com>
2026-01-26 11:50:00 +0900

    Add files via upload

---
+++ character.js
@@ -0,0 +1,23 @@
+// URLから ?id=xxx を取得
+const params = new URLSearchParams(location.search);
+const id = params.get("id");
+
+// キャラデータ（ここ増やすだけ）
+const characters = {
+  ULTRAMAN_ZERO: {
+    name: "ウルトラマン",
+    introduction: "タロウの息子"
+  },
+  ULTRAMAN_TRO: {
+    name: "ウルトラセブン",
+    introduction: "テクニカルで上級者向け。"
+  }
+};
+
+// 表示処理
+if (characters[id]) {
+  document.getElementById("name").textContent = characters[id].name;
+  document.getElementById("introduction").textContent = characters[id].introduction;
+} else {
+  document.getElementById("name").textContent = "キャラが見つかりません";
+}
