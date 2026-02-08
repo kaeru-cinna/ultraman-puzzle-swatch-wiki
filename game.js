9d28abcb491c27c33acb361f1591cb790d89354f
kaeru-cinna <yosiki.4979@gmail.com>
2026-02-09 01:27:37 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -15,9 +15,9 @@ let lastSwapped = null;
 
 class Cell {
   constructor(type="normal",color=null,bombType=null){
-    this.type=type; // normal / bomb
+    this.type=type; // normal bomb
     this.color=color;
-    this.bombType=bombType; // lineH lineV explode color random
+    this.bombType=bombType;
     this.highlight=false;
   }
 }
@@ -55,22 +55,39 @@ function draw(){
       }
 
       if(cell.type==="bomb"){
-        ctx.fillStyle=cell.color || "white";
-        drawStar(x,y,CELL/2-6,5);
+        let color="orange";
+        if(cell.bombType==="random") color="gray";
+        if(cell.bombType==="explode") color="black";
+        if(cell.bombType==="color"){
+          drawRainbowTriangle(x,y,CELL/2-6);
+          continue;
+        }
+        ctx.fillStyle=color;
+        drawTriangle(x,y,CELL/2-6);
       }
     }
   }
 }
 
-function drawStar(cx,cy,r,points){
+function drawTriangle(cx,cy,r){
   ctx.beginPath();
-  for(let i=0;i<points*2;i++){
-    let angle=Math.PI*i/points;
-    let rad=i%2===0?r:r/2;
-    ctx.lineTo(cx+rad*Math.sin(angle),cy-rad*Math.cos(angle));
-  }
+  ctx.moveTo(cx,cy-r);
+  ctx.lineTo(cx-r,cy+r);
+  ctx.lineTo(cx+r,cy+r);
   ctx.closePath();
   ctx.fill();
+}
+
+function drawRainbowTriangle(cx,cy,r){
+  let g=ctx.createLinearGradient(cx-r,cy-r,cx+r,cy+r);
+  g.addColorStop(0,"red");
+  g.addColorStop(0.2,"orange");
+  g.addColorStop(0.4,"yellow");
+  g.addColorStop(0.6,"green");
+  g.addColorStop(0.8,"blue");
+  g.addColorStop(1,"purple");
+  ctx.fillStyle=g;
+  drawTriangle(cx,cy,r);
 }
 
 function startGame(){
@@ -84,82 +101,107 @@ function swap(r1,c1,r2,c2){
   let tmp=board[r1][c1];
   board[r1][c1]=board[r2][c2];
   board[r2][c2]=tmp;
-  lastSwapped=[r2,c2]; // Swipe先
+  lastSwapped=[r2,c2];
 }
 
 function checkMatches(){
-  let matches=[];
-  let clearSet=new Set();
 
-  // 横
+  let mark = Array.from({length:SIZE},()=>Array(SIZE).fill(false));
+  let bombInfo=null;
+
+  // 横チェック
   for(let r=0;r<SIZE;r++){
     let count=1;
     for(let c=1;c<=SIZE;c++){
-      if(c<SIZE && board[r][c] && board[r][c-1] &&
+      if(c<SIZE &&
+         board[r][c] &&
+         board[r][c-1] &&
          board[r][c].color===board[r][c-1].color){
         count++;
       }else{
         if(count>=3){
-          let group=[];
           for(let k=0;k<count;k++){
-            group.push([r,c-1-k]);
-            clearSet.add(r+","+(c-1-k));
+            mark[r][c-1-k]=true;
           }
-          matches.push(group);
+          if(count===4) bombInfo="line";
+          if(count>=5) bombInfo="color";
         }
         count=1;
       }
     }
   }
 
-  // 縦
+  // 縦チェック
   for(let c=0;c<SIZE;c++){
     let count=1;
     for(let r=1;r<=SIZE;r++){
-      if(r<SIZE && board[r][c] && board[r-1][c] &&
+      if(r<SIZE &&
+         board[r][c] &&
+         board[r-1][c] &&
          board[r][c].color===board[r-1][c].color){
         count++;
       }else{
         if(count>=3){
-          let group=[];
           for(let k=0;k<count;k++){
-            group.push([r-1-k,c]);
-            clearSet.add((r-1-k)+","+c);
+            mark[r-1-k][c]=true;
           }
-          matches.push(group);
+          if(count===4) bombInfo="line";
+          if(count>=5) bombInfo="color";
         }
         count=1;
       }
     }
   }
 
+  // 2x2 ランダムボム
+  for(let r=0;r<SIZE-1;r++){
+    for(let c=0;c<SIZE-1;c++){
+      let a=board[r][c];
+      let b=board[r][c+1];
+      let d=board[r+1][c];
+      let e=board[r+1][c+1];
+      if(a&&b&&d&&e &&
+         a.color===b.color &&
+         a.color===d.color &&
+         a.color===e.color){
+        mark[r][c]=mark[r][c+1]=mark[r+1][c]=mark[r+1][c+1]=true;
+        bombInfo="random";
+      }
+    }
+  }
+
+  let clearSet=new Set();
+  for(let r=0;r<SIZE;r++){
+    for(let c=0;c<SIZE;c++){
+      if(mark[r][c]) clearSet.add(r+","+c);
+    }
+  }
+
   if(clearSet.size>0){
-    generateBomb(matches);
+    generateBomb(bombInfo);
     clearMatches(clearSet);
   }
 }
 
-function generateBomb(matches){
-  if(!lastSwapped) return;
+function generateBomb(type){
+  if(!type || !lastSwapped) return;
+  let [r,c]=lastSwapped;
 
-  for(let group of matches){
-    if(group.length===4){
-      let [r,c]=lastSwapped;
-      let dir = group[0][0]===group[1][0]?"lineH":"lineV";
-      board[r][c]=new Cell("bomb",randomColor(),dir);
-    }
-    if(group.length===5){
-      let [r,c]=lastSwapped;
-      board[r][c]=new Cell("bomb",null,"color");
-    }
-  }
+  if(type==="line")
+    board[r][c]=new Cell("bomb",null,"lineH");
+
+  if(type==="color")
+    board[r][c]=new Cell("bomb",null,"color");
+
+  if(type==="random")
+    board[r][c]=new Cell("bomb",null,"random");
 }
 
 function clearMatches(clearSet){
 
   clearSet.forEach(key=>{
     let [r,c]=key.split(",").map(Number);
-    if(board[r][c]) board[r][c].highlight=true;
+    board[r][c].highlight=true;
   });
 
   draw();
@@ -168,6 +210,7 @@ function clearMatches(clearSet){
 
     clearSet.forEach(key=>{
       let [r,c]=key.split(",").map(Number);
+      if(lastSwapped && r===lastSwapped[0] && c===lastSwapped[1]) return;
       board[r][c]=null;
     });
 
@@ -207,58 +250,10 @@ function dropUp(){
 function refillBottom(){
   for(let c=0;c<SIZE;c++){
     for(let r=SIZE-1;r>=0;r--){
-      if(board[r][c]===null){
+      if(board[r][c]===null)
         board[r][c]=new Cell("normal",randomColor());
-      }
     }
   }
-}
-
-function activateBomb(r,c){
-  let bomb=board[r][c];
-  if(!bomb || bomb.type!=="bomb") return;
-
-  let clearSet=new Set();
-
-  if(bomb.bombType==="lineH"){
-    for(let i=0;i<SIZE;i++) clearSet.add(r+","+i);
-  }
-
-  if(bomb.bombType==="lineV"){
-    for(let i=0;i<SIZE;i++) clearSet.add(i+","+c);
-  }
-
-  if(bomb.bombType==="explode"){
-    for(let dr=-1;dr<=1;dr++){
-      for(let dc=-1;dc<=1;dc++){
-        let nr=r+dr,nc=c+dc;
-        if(nr>=0&&nr<SIZE&&nc>=0&&nc<SIZE)
-          clearSet.add(nr+","+nc);
-      }
-    }
-  }
-
-  if(bomb.bombType==="color"){
-    let colorCount={};
-    for(let r2=0;r2<SIZE;r2++){
-      for(let c2=0;c2<SIZE;c2++){
-        let cell=board[r2][c2];
-        if(cell && cell.color){
-          colorCount[cell.color]=(colorCount[cell.color]||0)+1;
-        }
-      }
-    }
-    let maxColor=Object.keys(colorCount)
-      .reduce((a,b)=>colorCount[a]>colorCount[b]?a:b);
-    for(let r2=0;r2<SIZE;r2++){
-      for(let c2=0;c2<SIZE;c2++){
-        if(board[r2][c2] && board[r2][c2].color===maxColor)
-          clearSet.add(r2+","+c2);
-      }
-    }
-  }
-
-  clearMatches(clearSet);
 }
 
 canvas.addEventListener("click",e=>{
@@ -268,15 +263,6 @@ canvas.addEventListener("click",e=>{
   let rect=canvas.getBoundingClientRect();
   let c=Math.floor((e.clientX-rect.left)/CELL);
   let r=Math.floor((e.clientY-rect.top)/CELL);
-
-  let cell=board[r][c];
-
-  if(cell && cell.type==="bomb"){
-    moves--;
-    document.getElementById("moves").textContent=moves;
-    activateBomb(r,c);
-    return;
-  }
 
   if(!selectedCell){
     selectedCell=[r,c];
