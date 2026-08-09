1cb1da08c06df6cbb800e6da63be44d4aab8e994
kaeru-cinna <yosiki.4979@gmail.com>
2026-08-09 23:02:57 +0900

    Add files via upload

--- DamageCalculation.js
+++ DamageCalculation.js
@@ -47,8 +47,8 @@ document.getElementById("scoreForm").addEventListener("submit", function(e) {
 
   if (maxHp > 0 && currentHp > 0) {
     let hpRate = currentHp / maxHp;
-    hpRate = Math.max(0, Math.min(1, hpRate));
     hpRate = Math.round(hpRate * 100) / 100;
+    hpRate = Math.max(0, Math.min(1, hpRate));
 
     const maxBonus = 0.5;
     const hpBonus = hpRate * maxBonus;
